d118c08fa8bc3181fcf15f04f35117c4f5aea73d
bbarni2020 <75606424+bbarni2020@users.noreply.github.com>
2022-11-28 19:14:49 +0100

    Add files via upload

--- fireconsole.js
+++ fireconsole.js
@@ -1,19 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDtK9A1EZGdG28qPbFrbU4E7lnhi5IK5Vc",
+  authDomain: "smartwidgets-website.firebaseapp.com",
+  projectId: "smartwidgets-website",
+  storageBucket: "smartwidgets-website.appspot.com",
+  messagingSenderId: "126534531673",
+  appId: "1:126534531673:web:3cb3ad3d702218566c0f48",
+  measurementId: "G-TXEHCJ8JF0"
+};
 
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
-
-
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyDtK9A1EZGdG28qPbFrbU4E7lnhi5IK5Vc",
-    authDomain: "smartwidgets-website.firebaseapp.com",
-    projectId: "smartwidgets-website",
-    storageBucket: "smartwidgets-website.appspot.com",
-    messagingSenderId: "126534531673",
-    appId: "1:126534531673:web:3cb3ad3d702218566c0f48",
-    measurementId: "G-TXEHCJ8JF0"
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
